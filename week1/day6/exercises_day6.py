695f200c820ef9c5c75d0f045efbbd4f78a91d60
ilan-uzan <ilanxuzan@gmail.com>
2025-08-14 22:13:15 +0300

    exercises day6

--- week1/day6/exercises_day6.py
+++ week1/day6/exercises_day6.py
@@ -120,19 +120,19 @@ print('Enjoy my loves <3')
 
 
 
-# Brand Information:
-
-# name: Zara
-# creation_date: 1975
-# creator_name: Amancio Ortega Gaona
-# type_of_clothes: men, women, children, home
-# international_competitors: Gap, H&M, Benetton
-# number_stores: 7000
-# major_color: 
-#     France: blue, 
-#     Spain: red, 
-#     US: pink, green
-
+Brand Information = {
+    'name': 'Zara', 
+    'creation_date': 1975,
+    'creator_name': 'Amancio Ortega Gaona',
+    'type_of_clothes': ['men', 'women', 'children', 'home'],
+    'international_competitors': ['Gap', 'H&M', 'Benetton'],
+    'number_stores': 7000,
+    'major_color': {
+        'France': 'blue', 
+        'Spain': 'red', 
+        'USA': ['pink', 'green']
+    }    
+}
 
 # Create a dictionary called brand with the provided data.
 # Modify and access the dictionary as follows:
