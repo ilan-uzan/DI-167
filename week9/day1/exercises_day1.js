f9f7d8ab3b77f59cd3a65e91e77ce73bf7f1e220
ilan-uzan <ilanxuzan@gmail.com>
2025-09-14 19:44:02 +0300

    learning advanced js

---
+++ week9/day1/exercises_day1.js
@@ -0,0 +1,10 @@
+// function checkYear(year) {
+//     if (year > 2000) {
+//         console.log('you are in the 21st century');
+//     } else {
+//         console.log('you live in the middle age');
+//     }
+// }
+
+// checkYear(2024); // expected oputut you live in the 21st century//
+// checkYear(1990); // expeced output you live in the middle age //
